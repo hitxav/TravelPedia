bcd187420809a6ae82bbf4695d7de4c02caaa663
Hitesh Bani <hitesh.bani24@gmail.com>
2024-01-11 19:04:36 +0530

    added side draw animation and few more updates for it

--- travelpedia/src/shared/components/Navigation/MainNavigationWA.js
+++ travelpedia/src/shared/components/Navigation/MainNavigationWA.js
@@ -10,26 +10,27 @@ import Backdrop from "../UIElements/Backdrop";
 function MainNavigation(props){
     const [drawerIsOpen, setDrawerIsOpen] = useState(false);
 
-    function openDrawerHandler(){
+    function openDrawer(){
         setDrawerIsOpen(true);
     }
 
-    function closeDrawerHandler(){
+    function closeDrawer(){
         setDrawerIsOpen(false);
     }
 
     return (
         <React.Fragment>
-            { drawerIsOpen ? <Backdrop onClick={closeDrawerHandler} /> : null}
-         
-            <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
-                <nav className="main-navigation__drawer-nav">
-                    <NavLinks/>
-                </nav>
-            </SideDrawer>
-
+            { drawerIsOpen ? <Backdrop onClick={closeDrawer} /> : null}
+            { drawerIsOpen ? 
+                <SideDrawer>
+                    <nav className="main-navigation__drawer-nav">
+                        <NavLinks/>
+                    </nav>
+                </SideDrawer> : null
+            }
+                  
             <MainHeader>
-                <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
+                <button className="main-navigation__menu-btn" onClick={openDrawer}>
                     <span />
                     <span />
                     <span />
--- travelpedia/src/shared/components/Navigation/SideDrawer.js
+++ travelpedia/src/shared/components/Navigation/SideDrawer.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { CSSTransition } from 'react-transition-group';
 
 import "./SideDrawer.css";
 
-function SideDrawer(props){
-    const content =  <aside className="side-drawer">{props.children}</aside>
+function SideDrawer(props){ // * adding onClick on aside for sideDrwaer  will close it if you click anywhere on side bar }
+    const content = (
+        <CSSTransition in={props.show} timeout={50} classNames="slide-in-left" mountOnEnter unmountOnExit>
+            <aside className="side-drawer" onClick={props.onClick}>{props.children}</aside> 
+        </CSSTransition>
+    );
     return ReactDOM.createPortal(content,document.getElementById("drawer-hook"));
 }
 
